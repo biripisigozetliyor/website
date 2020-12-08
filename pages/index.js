cbe0aed9511da4e0b1471615f6772cb02d1de435
Kenan Günen <gunenkenan@gmail.com>
2020-12-08 14:38:22 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Layout from "../components/layout"
+import Head from "next/head"
+
+function HomePage() {
+  return (
+    <Layout>
+      <Head>
+        <title>Anasayfa</title>
+      </Head>
+      <h1>Home Page</h1>
+    </Layout>
+  )
+}
+
+export default HomePage
