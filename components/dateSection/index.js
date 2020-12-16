8ae3e11ea73290a7cc93a3d54a314baff312bbd1
kenangunen <gunenkenan@gmail.com>
2020-12-16 20:35:18 +0300

    some responsive fix

--- components/dateSection/index.js
+++ components/dateSection/index.js
@@ -8,43 +8,45 @@ import Button from "../button"
 const DateSection = () => {
   return (
     <div className="date">
-      <div className="selectPet">
-        <Button type="square" value="Kedi" />
-        <Button type="square" value="Kuş" />
-        <Button type="square" value="Balık" />
-        <Button type="square" value="Sürüngen" />
-        <Button type="square" value="Kemirgen" />
-      </div>
-      <div className="form">
-        <div className="ddWrapper">
-          <div class="dropdown">
-            <div className="ddType">
-              <span>Tarih Seçiniz</span>
-              <Calander />
+      <div className="wrapper">
+        <div className="selectPet">
+          <Button type="square" value="Kedi" />
+          <Button type="square" value="Kuş" />
+          <Button type="square" value="Balık" />
+          <Button type="square" value="Sürüngen" />
+          <Button type="square" value="Kemirgen" />
+        </div>
+        <div className="form">
+          <div className="ddWrapper">
+            <div class="dropdown">
+              <div className="ddType">
+                <span>Tarih Seçiniz</span>
+                <Calander />
+              </div>
+              <div class="dropdown-content">
+                <p>Hello World!</p>
+              </div>
             </div>
-            <div class="dropdown-content">
-              <p>Hello World!</p>
+            <div class="dropdown">
+              <div className="ddType">
+                <span>Lütfen konum seçiniz</span>
+                <Location />
+              </div>
+              <div class="dropdown-content">
+                <p>Hello World!</p>
+              </div>
             </div>
           </div>
-          <div class="dropdown">
-            <div className="ddType">
-              <span>Lütfen konum seçiniz</span>
-              <Location />
-            </div>
-            <div class="dropdown-content">
-              <p>Hello World!</p>
-            </div>
+          <div className="btn">
+            <Button type="iconicBorder" value="Randevu Al" icon="smallPaw" />
           </div>
         </div>
-        <div className="btn">
-          <Button type="iconicBorder" value="Randevu Al" icon="smallPaw" />
+        <div className="leftSide">
+          <LeftPaw />
         </div>
-      </div>
-      <div className="leftSide">
-        <LeftPaw />
-      </div>
-      <div className="rightSide">
-        <RighttPaw />
+        <div className="rightSide">
+          <RighttPaw />
+        </div>
       </div>
     </div>
   )
